40b8436fa9554b7de6d511202ac6f6142b89c45f
Chinito <clark.wayne023@gmail.com>
2024-06-13 00:38:35 +0800

    Fix Feature Card & Set Dark as Default Theme

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -7,10 +7,21 @@ import { ProfilePhoto } from "@/components/component/profile-photo";
 import { FeaturedWorkBadge } from "@/components/component/featured-work-badge";
 import Link from "next/link";
 import { FeaturedWorkCard } from "@/components/component/featured-work-card";
+import caveWebsite from "@/assets/cave-website.jpg";
+import leafletWebsite from "@/assets/leaflet-website.jpg";
 
 export default function Home() {
+  const imageArray = [
+    caveWebsite,
+    leafletWebsite,
+    caveWebsite,
+    leafletWebsite,
+    caveWebsite,
+    leafletWebsite,
+  ];
+
   return (
-    <main className="relative flex flex-col h-dvh w-full items-center justify-start bg-white dark:bg-[#0D0D11] md:shadow-xl z-10 gap-10">
+    <main className="relative flex flex-col h-dvh w-full items-center justify-start bg-white dark:bg-[#0D0D11] z-10 gap-4">
       <section className="w-full max-w-[740px] pt-36 px-4 flex flex-col justify-start items-start">
         <motion.div
           initial={{ opacity: "0%" }}
@@ -26,9 +37,8 @@ export default function Home() {
               transition={{ duration: 0.5, delay: 2.5 }}
               className=""
             >
-              I am a developer who loves to build things for the web. I
-              specialize in creating fast, responsive, and accessible websites.
-              I am currently building at{" "}
+              I love to build things for the web. I specialize in creating fast,
+              responsive, and accessible websites. I am currently building at{" "}
               <Link
                 href="https://cave-wine.vercel.app"
                 className="text-blue-500 font-semibold"
@@ -40,18 +50,22 @@ export default function Home() {
           </div>
         </motion.div>
       </section>
-      <section className="max-w-[740px] w-full px-4">
+      <section className="max-w-[740px] w-full">
         <motion.div
           initial={{ opacity: "0%" }}
           animate={{ opacity: "100%" }}
           transition={{ duration: 0.5, delay: 2.5 }}
-          className="font-normal text-sm flex flex-col justify-start items-start gap-6"
+          className="font-normal text-sm flex flex-col justify-start items-start gap-6 mb-10"
         >
           <Link href="#" className="cursor-pointer">
             <FeaturedWorkBadge />
           </Link>
-          <div>
-            <FeaturedWorkCard />
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center items-center w-full max-w-[740px]">
+            {Array.from({ length: 6 }).map((_, i) => (
+              <div key={i}>
+                <FeaturedWorkCard image={imageArray[i]} />
+              </div>
+            ))}
           </div>
         </motion.div>
       </section>
--- src/components/component/featured-work-card.tsx
+++ src/components/component/featured-work-card.tsx
@@ -1,23 +1,30 @@
 "use client";
 import React from "react";
 import { PinContainer } from "../ui/3d-pin";
-import Image from "next/image";
-import cave from "@/assets/cave-website.jpg";
+import Image, { StaticImageData } from "next/image";
 import { motion } from "framer-motion";
 
-export function FeaturedWorkCard() {
+export function FeaturedWorkCard({ image }: { image: StaticImageData }) {
   return (
     <motion.div
       initial={{ opacity: "0%" }}
       animate={{ opacity: "100%" }}
       transition={{ duration: 0.5, delay: 2.5 }}
-      className="h-full w-full flex items-start justify-center "
+      className="h-full w-full flex items-start justify-center"
     >
       <PinContainer
         title="Cave Technologies"
         href="https://cave-wine.vercel.app"
+        className="p-0"
       >
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem]">
+        <Image
+          className="rounded-lg"
+          alt="Cave Technologies Website"
+          width={1000}
+          height={600}
+          src={image.src}
+        />
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem]">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-black dark:text-slate-100">
             Cave Technologies
           </h3>
@@ -26,14 +33,6 @@ export function FeaturedWorkCard() {
               We offer seamless websites, putting businesses online.
             </span>
           </div>
-
-          <Image
-            className="rounded-lg mt-4"
-            alt="Cave Technologies Website"
-            width={1000}
-            height={600}
-            src={cave.src}
-          />
         </div>
       </PinContainer>
     </motion.div>
